bca45d70086ab1543d7f1ab254121b1ff98c3c80
Andrey Dmitrievich Grishilo <medved2309@yandex.ru>
2022-11-19 02:12:41 +0500

    reshenie zadacha 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,42 @@
 import main.java.basket.Basket;
 import main.java.clientLog.ClientLog;
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
-import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException, XPathExpressionException, SAXException, ParserConfigurationException {
 
         String[] products = {"Молоко", "Хлеб", "Гречневая крупа", "Курица", "Яйца куринные"};
         int[] prices = {50, 14, 80, 100, 90};
 
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("shop.xml");
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String name = xPath.compile("/shop/load/fileName").evaluate(doc);
+//        System.out.println(name);
+        boolean loadFileEnabled = Boolean.parseBoolean(xPath.compile("/shop/load/enabled").evaluate(doc));
+        boolean saveLog = Boolean.parseBoolean(xPath.compile("/shop/log/enabled").evaluate(doc));
+
         //Basket basket = new Basket(products, prices);
         Basket basket2 = Basket.loadFromJSON();
 
-        if (basket2 == null) {
-            basket2 = new Basket(products, prices);
-        } else {
+        if (loadFileEnabled) {
             basket2 = Basket.loadFromJSON();
+        } else {
+            basket2 = new Basket(products, prices);
         }
 
         Scanner scanner = new Scanner(System.in);
@@ -58,11 +75,14 @@ public class Main {
                 }
                 basket2.addToCart(productNumber, productCount);
                 ClientLog clientLog = new ClientLog(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
-                clientLog.log();
+                if (saveLog) {
+                    clientLog.log();
+                }
+
 
             }
         }
-      //  basket2.saveTxt(new File("basket.txt"));
+        //  basket2.saveTxt(new File("basket.txt"));
 //        basket2.saveTxt(new File("basket.txt"));
 //        basket2.saveJSON(new File("basket.json"));
         basket2.saveJSON();
